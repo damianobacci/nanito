a18ac56255ccc9dd1c42436d13beb57a358c3ccd
Damiano Bacci <damiano.bacci@gmail.com>
2023-11-14 19:23:21 +0100

    Sound playing

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Pressable, Image } from "react-native";
+import { Audio } from "expo-av";
+import { useState, useEffect } from "react";
 
 export default function App() {
+  // const [sound, setSound] = useState();
+
+  async function playSound() {
+    const { sound } = await Audio.Sound.createAsync(
+      require("./assets/sounds/infatti.mp3")
+    );
+    // setSound(sound);
+
+    await sound.playAsync();
+  }
+
+  // useEffect(() => {
+  //   return sound
+  //     ? () => {
+  //         console.log("Unloading Sound");
+  //         sound.unloadAsync();
+  //       }
+  //     : undefined;
+  // }, [sound]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ fontSize: 40, marginBottom: 50 }}>Nanito App</Text>
+      <View style={styles.image}>
+        <Pressable onPress={playSound}>
+          <Image source={require("./assets/images/nano-emoji.png")} />
+        </Pressable>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 120,
+    flexDirection: "column",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  image: {},
 });
